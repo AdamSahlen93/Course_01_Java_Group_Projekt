4ebf07f45a621b56c9fe54196109fb0c592c9745
Fat-stack <robinnilsson5@msn.com>
2020-11-04 14:05:07 +0100

    added method in main

--- src/projectpart3train/Main.java
+++ src/projectpart3train/Main.java
@@ -5,7 +5,9 @@ public class Main
 
     public static void main(String[] args)
     {
-        // initiera menu from class Menu.
+        // initiera menu() from class Menu.
+        Menu menu = new Menu();
+        menu.menu();
     }
 
 }
