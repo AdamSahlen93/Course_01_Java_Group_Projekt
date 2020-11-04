9b570f1597eeb3223f92384c60c29859c2c9bd48
Fat-stack <robinnilsson5@msn.com>
2020-11-04 10:35:33 +0100

    Added attributes to Passenger

--- src/projectpart3train/Passenger.java
+++ src/projectpart3train/Passenger.java
@@ -3,6 +3,52 @@ package projectpart3train;
 public class Passenger
 {
 
-//Allt som har med Passenger att göra.
+int age;
+String firstname;
+String lastName;
+String fullName;
 
+                    /*KONSTRUKTOR*/
+    public Passenger(int age, String firstname, String lastName, String fullName)
+    {
+        this.age = age;
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.fullName = fullName;
+    }
+
+                    /* SETTERS*/
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+                     /* GETTERS*/
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
 }
