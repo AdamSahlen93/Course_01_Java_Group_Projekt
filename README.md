489634e7474f6ac99d529382a7f2e22fd40d4abd
AdamSahlen93 <72616481+AdamSahlen93@users.noreply.github.com>
2020-11-04 09:34:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjektDel3.Grupp5
+ Gemensamt projekt grupp 5 
---
+++ src/projectpart3train/Main.java
@@ -0,0 +1,11 @@
+package projectpart3train;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        
+    }
+
+}
