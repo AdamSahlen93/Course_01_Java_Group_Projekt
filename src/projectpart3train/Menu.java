7d8dbfac32ce6fe0128399416299d2dcc7322ee4
Simon Sahlin <simon.sahlin@utb.ecutbildning.se>
2020-11-04 12:00:58 +0100

    Updated Menu

--- src/projectpart3train/Menu.java
+++ src/projectpart3train/Menu.java
@@ -1,5 +1,34 @@
 package projectpart3train;
 
+import java.util.Scanner;
+
 public class Menu {
 
+	int whileLoop;
+	
+	public void menu () {
+		
+		while (whileLoop != 99) {
+			Scanner scanner = new Scanner(System.in);
+			int choice = scanner.nextInt();
+			//Wanna try to make a try catch loop to catch exceptions like letters instead of numbers? 
+			
+			switch (choice) {
+			case 1: 
+				// Start a method to run the program.
+				break;
+			case 2: 
+				// Print a list of passengers.
+				break;
+			case 3: 
+				// Choice to end the program.
+			
+			}
+			
+		}
+		
+		
+		
+	}
+	
 }
