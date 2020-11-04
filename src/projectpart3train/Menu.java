27b029ffa4892220fa91f9ce97937ae5b648d932
Simon Sahlin <simon.sahlin@utb.ecutbildning.se>
2020-11-04 13:59:41 +0100

    Changes made in menu

--- src/projectpart3train/Menu.java
+++ src/projectpart3train/Menu.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 
 public class Menu {
 
+	Traveler traveler = new Traveler();
 	int whileLoop;
 	
 	public void menu () {
 		
 		while (whileLoop != 99) {
 			Scanner scanner = new Scanner(System.in);
+			System.out.println("-----------Menu-----------");
+			System.out.println("Choose one of the options below:\n1. Register a new passenger.\n2. Print a list of all the passengers onboard the train.\n3. Close the program.");
 			int choice = scanner.nextInt();
 			
 			//Wanna try to make a try catch loop to catch exceptions like letters instead of numbers? 
 			
 			switch (choice) {
 			case 1: 
+				traveler.createNewPassenger();
 				// Start a method to run the program.
 				break;
 			case 2: 
@@ -23,12 +27,11 @@ public class Menu {
 				break;
 			case 3: 
 				// Choice to end the program.
+				whileLoop = 99; // Closes the while-loop
 			
 			}
 			
 		}
-		
-		
 		
 	}
 	
