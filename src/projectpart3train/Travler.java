609fd7a3dd08ff2e9561aeb45225cf64c7ecb617
Simon Sahlin <simon.sahlin@utb.ecutbildning.se>
2020-11-04 14:39:06 +0100

    Update Travler.java

--- src/projectpart3train/Travler.java
+++ src/projectpart3train/Travler.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner; 
 
 public class Travler {
-	
+		
 	ArrayList <Passenger> passengerList = new ArrayList <Passenger>();
 	
     public void createNewPassenger() {
@@ -13,9 +13,13 @@ public class Travler {
     	String firstName = scanner.nextLine();
     	System.out.println("What is your last name?");
     	String lastName = scanner.nextLine();
-    	Passenger passenger = new Passenger(0, firstName, lastName, "");
-    	passengerList.add
+    	String fullName = firstName + " " + lastName;
+    	Passenger passenger = new Passenger(0, firstName, lastName, fullName);
+    	passengerList.add(passenger);
+    	
+    	System.out.println("I need to know how old you are to know what price you will pay.\nHow old are you? ");
+    	int age = scanner.nextInt();
+    	// Here goes the loop to check for age, and what price to pay.
     	
     }
-    
 }
