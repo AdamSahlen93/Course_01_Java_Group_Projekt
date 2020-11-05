a09c65730b80fe1ada674491026fe7ecfd5203c3
Fat-stack <robinnilsson5@msn.com>
2020-11-05 16:52:20 +0100

    merge of Interface

--- src/projectpart3train/Interface.java
+++ src/projectpart3train/Interface.java
@@ -2,12 +2,10 @@ package projectpart3train;
 
 interface Interface {
 
-<<<<<<< HEAD
-    // public void createPassenger(); -> etc.
-=======
+
 	public void createPassenger(String firstName, String lastName, int age);
     // public void createPassenger(); -> etc. 
->>>>>>> f44106b6e5967bcd4f0046ace00cecb7d6984a30
+
     // This is where something happens thad I dont quite get.. /RN
 
 }
