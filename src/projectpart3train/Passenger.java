8645de339a92ff3990a4471deadea4853d2b673c
Fat-stack <robinnilsson5@msn.com>
2020-11-05 16:09:08 +0100

    uppdated Passenger/ added InterfaceTraveler

--- src/projectpart3train/Passenger.java
+++ src/projectpart3train/Passenger.java
@@ -1,11 +1,55 @@
 package projectpart3train;
+//Creates the tickets by checking the info in travelers
+//Alt: Creates the traveler in this class instead: with passenger as interface - Bins Travelers
 
-public interface Passenger
+public class Passenger
 {
 
-    //Creates the tickets by checking the info in travelers
-    //Alt: Creates the traveler in this class instead: with passenger as interface - Bins Travelers
+        /*Attributes for Passenger*/
+private int age;
+private String firstName;
+private String lastName;
+private String fullName;
 
+         /* Construktor to be inserted here, depending on what parameters we will use.*/
+            /* No constructor at the moment.*/
+
+
+
+        /*SETTERS*/
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    /*GETTERS */
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
 }
 
 
---
+++ src/projectpart3train/TravelerInterface.java
@@ -0,0 +1,6 @@
+package projectpart3train;
+
+public interface TravelerInterface {
+    // This is where something happens thad I dont quite get.. /RN
+
+}
